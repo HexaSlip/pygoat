cef19bafc1c5760e7587500e659a4cdc807ee6f5
Mia <miriah.picinich@gmail.com>
2026-02-24 19:28:05 -0500

    Update views.py

--- introduction/views.py
+++ introduction/views.py
@@ -459,7 +459,7 @@ def cmd_lab2(request):
             print(val)
             try:
                 output = ast.literal_eval(val)
-			except Exception:
+			except:
                 output = "Something went wrong"
                 return render(request,'Lab/CMD/cmd_lab2.html',{"output":output})
             print("Output = ", output)
